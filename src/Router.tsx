02d23b3d6654b7b7ff05c5c27ac680aecc149d9a
erickhenri <herick.henri16@gmail.com>
2022-10-12 14:40:22 -0300

    solved error import

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Home } from "./Pages/Home/Index";
+import { Home } from "./Pages/Home/index";
 import { HelloWord } from "./projects/day2/HelloWord";
 
 export function Router() {
